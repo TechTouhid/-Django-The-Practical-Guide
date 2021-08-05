674c1e9e29ff81076ff4c7b46b299944973f3dd3
touhidalamin123@gmail.com <touhidalamin123@gmail.com>
2021-08-05 16:27:22 +0600

    Section 5 done book_store app with django Admin

--- project3_book_store/book_store/book_outlet/admin.py
+++ project3_book_store/book_store/book_outlet/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Book
+
 
 # Register your models here.
+
+class BookAdmin(admin.ModelAdmin):
+    # readonly_fields = ("slug",)
+    prepopulated_fields = {"slug": ("title",)}  # This field can make slug automatically
+    list_filter = ("author", "rating")  # This field can add filters in the admin 
+    list_display = ("title", "author", "rating")
+
+admin.site.register(Book, BookAdmin)
--- project3_book_store/book_store/book_outlet/models.py
+++ project3_book_store/book_store/book_outlet/models.py
@@ -11,7 +11,7 @@ class Book(models.Model):
     rating = models.IntegerField(validators=[MaxValueValidator(5), MinValueValidator(1)])
     author = models.CharField(null=True, max_length=100)
     is_bestselling = models.BooleanField(default=False)
-    slug = models.SlugField(default="", null=False,
+    slug = models.SlugField(default="", null=False, blank=True,
                             db_index=True)  # Harry Potter => harry-potter-1, db_index can improve the performance
 
     def __str__(self):
@@ -20,6 +20,7 @@ class Book(models.Model):
     def get_absolute_url(self):
         return reverse("book_detail", args=[self.slug])
 
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.title)
-        super().save(*args, **kwargs)
+    # we added a prepopulated_fields = {"slug": ("title",)} in admin so we don't need this function now
+    # def save(self, *args, **kwargs):
+    #     self.slug = slugify(self.title)
+    #     super().save(*args, **kwargs)
